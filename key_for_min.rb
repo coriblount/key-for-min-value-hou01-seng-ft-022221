0741c42e3f36c4b703a3028fedbdaf58e46d94bb
cori <heycorihey@icloud.com>
2021-02-11 05:56:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
-  hash.each do |key, value|
+  hash.each do |k, v|
     if lowest_value == nil 
-      lowest_value = value
-      lowest_key = key
+      lowest_value = v
+      lowest_key = k
     end
   end
 lowest_key
